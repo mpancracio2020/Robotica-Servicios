0de9cb7883a63c0f8cf7800b65ee33b5b856c258
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2024-01-16 23:53:53 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -108,13 +108,16 @@ export default function Projects() {
                       <span></span>
                     {"\n"}
                     {"\n"}
+                    Here is an example video: {"\n"} {"\n"}
+                    <center><iframe width="560" height="315" src="https://www.youtube.com/embed/pWLZbrcnBuA?si=FN8QzS648ruP2o_T" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </center>
+                  <span></span>
 
-                        Here is an example video: {"\n"} {"\n"}
-                    <center><iframe width="560" height="315" src="https://www.youtube.com/embed/GbOMlcvZUxs?si=nZPCvAFe2eF66ALc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
                     
+                    </pre>
                   </p>
-
-                                  } />
+                
+                    } />
+                 
               ))}
             </div>
           </div>
