74f47f8eff40c16c261e340d70dceb343f838eda
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2024-01-17 09:11:54 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -126,7 +126,7 @@ export default function Projects() {
                     
                     The movement rule that the algorithm follows is the following:
                       <center>
-                      <img src={mov} width={150} height={150} alt='Large Pizza' />
+                      <img src={mov} width={650} height={150} alt='Large Pizza' />
                     </center>  
 
                     {"\n"}
